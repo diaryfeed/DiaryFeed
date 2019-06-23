0a750848555cb591df3c4609bfb1588c2c975e91
hp <reeteshkumarmandal@gmail.com>
2019-06-23 19:34:54 +0530

    blog detail and comment activity

---
+++ app/src/main/java/com/example/reetesh_ranjan/diaryfeed/BlogDetail.java
@@ -0,0 +1,13 @@
+package com.example.reetesh_ranjan.diaryfeed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlogDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_detail);
+    }
+}
